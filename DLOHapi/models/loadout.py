8594b2d3cf5f407db5e17e3e66d27a0c25855a30
Travis Hall <t.hall89d@gmail.com>
2021-12-08 17:29:19 -0500

    finalized models?

--- DLOHapi/models/loadout.py
+++ DLOHapi/models/loadout.py
@@ -1,8 +1,10 @@
 from django.db import models
+from DLOHapi.models.destiny_inv import DestinyInventoryItems
 from DLOHapi.models.dloh_user import DlohUser
 
 
 class Loadout(models.Model):
     name = models.CharField(max_length=50)
     dloh_user = models.ForeignKey(DlohUser, on_delete=models.CASCADE)
-    
+    destiny_items_list = models.ManyToManyField(
+        DestinyInventoryItems, through="LoadoutInv", related_name="loadouts_list")
