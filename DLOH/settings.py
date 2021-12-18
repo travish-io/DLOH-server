3278d01a2924f8f98a94fafdd7137aca132730f9
Travis Hall <t.hall89d@gmail.com>
2021-12-17 20:49:42 -0500

    whitelist

--- DLOH/settings.py
+++ DLOH/settings.py
@@ -57,11 +57,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://localhost:3000',
-    'https://127.0.0.1:3000',
-    'https://dloh.herokuapp.com/',
+    'https://www.bungie.net/',
     'https://destinyloadouthelper.herokuapp.com/'
 )
 
