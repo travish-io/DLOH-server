b308db8485a110f9a91d5be8b8062aadd5ea64a1
Travis Hall <t.hall89d@gmail.com>
2021-12-20 16:07:15 -0500

    added localhost back to cors wl

--- DLOH/settings.py
+++ DLOH/settings.py
@@ -57,6 +57,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
     'https://www.bungie.net/',
     'https://destinyloadouthelper.herokuapp.com/'
 )
